148168956fc4fe04b9df0e65d4013f59773c86f5
Sina Sahabi <sinasahabi1@gmail.com>
2024-06-28 21:02:36 +0330

    add models

--- src/group7/models.py
+++ src/group7/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Word(models.Model):
+    str = models.CharField(max_length=10)
+
+class WordelGameSession(models.Model):
+    numberOfTries = models.IntegerField
+    str = models.CharField(max_length=10)
--- src/group7/views.py
+++ src/group7/views.py
@@ -1,10 +1,18 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
 def home(request):
-	template = loader.get_template('home.html')
-	return HttpResponse(template.render())
+    if request.method=='GET':
+        template = loader.get_template('index.html')
+        return HttpResponse(template.render())
+    return render (request,'index.html')
 
 def test(request):
-	template = loader.get_template('test.html')
-	return HttpResponse(template.render())
+    template = loader.get_template('test.html')
+    return HttpResponse(template.render())
+
+def newWordel(request):
+    if request.method=='GET':
+        template = loader.get_template('??.html')
+        return HttpResponse(template.render())
