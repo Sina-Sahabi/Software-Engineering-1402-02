08b0361b6e26d8fcde43adf65548ca448202ed52
Sina Sahabi <sinasahabi1@gmail.com>
2024-06-28 19:21:01 +0330

    khodaya base dige

--- src/group7/templates/src/components/Pages/HangMan/HangMan.jsx
+++
@@ -1,3 +0,0 @@
-export function Hangman(){
-    return(<>hangman</>);
-}
--- src/group7/views.py
+++ src/group7/views.py
@@ -2,5 +2,9 @@ from django.http import HttpResponse
 from django.template import loader
 
 def home(request):
-	template = loader.get_template('index.html')
+	template = loader.get_template('home.html')
+	return HttpResponse(template.render())
+
+def test(request):
+	template = loader.get_template('test.html')
 	return HttpResponse(template.render())
