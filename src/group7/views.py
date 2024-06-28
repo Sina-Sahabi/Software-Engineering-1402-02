31388e735cb0ed27a1d1c7acf626a724ae28c8b6
mahya <beheshtimahya11@gmail.com>
2024-06-28 21:01:41 +0330

    wordle fixed

--- src/group7/views.py
+++ src/group7/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.template import loader
 
 def home(request):
-	template = loader.get_template('home.html')
+	template = loader.get_template('index.html')
 	return HttpResponse(template.render())
 
 def test(request):
