619ed6c2e9819430c78b78fb8a1cad1b615861e0
mahya <beheshtimahya11@gmail.com>
2024-06-28 15:16:55 +0330

    wordle initial ui

---
+++ src/group7/templates/src/colors.js
@@ -0,0 +1,17 @@
+const colors = {
+    navbar: "#EBD9FC", //nav
+    background: "#F2EBFB", // background
+    textPrimary: "#FBFAFF", // cards text
+    textSecondary: "#6b5598", // medium grey
+    buttonText: "#6b5598", // button text
+    buttonHover: "#6b5598", // light grey
+    mainCard: "#D4BBFC", // mainCard
+    cards: "#F2EBFB", //cards
+    button: "#EBD9FC",
+    green: "#70e000",
+    yellow: "#f9a620",
+    gray:"#cfdbd5",
+    wordleDefault: "#9985b445"
+};
+
+export default colors;
