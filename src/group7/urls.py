24da97c66fe1cb7de14deafc9919208e7037602c
Sina Sahabi <sinasahabi1@gmail.com>
2024-06-29 02:28:12 +0330

    redirect

--- src/group7/urls.py
+++ src/group7/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-
+from . import views
 
 urlpatterns = [
-
-] 
+    path('/', views.home, name='home page'),
+    # path('newGame/wordel', views.newWordel, name='create new Wordel game'),
+    # path('newGame/hangman', views.newHangman, name='create new Hangman game'),
+    path("<path:req_path>/", views.redirect_view),
+]
--- src/group7/views.py
+++ src/group7/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+import requests
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+def home(request):
+    return HttpResponse("ok")
+
+@csrf_exempt
+def redirect_view(request, req_path):
+    target_url = 'https://example.com/' + req_path
+
+    headers = {key: value for key, value in request.headers.items()}
+
+    if request.method == 'GET':
+        response = requests.get(target_url, headers=headers, params=request.GET)
+    elif request.method == 'POST':
+        response = requests.post(target_url, headers=headers, data=request.POST)
+    elif request.method == 'PUT':
+        response = requests.put(target_url, headers=headers, data=request.body)
+    elif request.method == 'DELETE':
+        response = requests.delete(target_url, headers=headers)
+    elif request.method == 'PATCH':
+        response = requests.patch(target_url, headers=headers, data=request.body)
+    elif request.method == 'OPTIONS':
+        response = requests.options(target_url, headers=headers)
+    else:
+        return HttpResponse('Method not supported', status=405)
+
+    django_response = HttpResponse(response.content, status=response.status_code)
+    for key, value in response.headers.items():
+        django_response[key] = value
+    
+    return django_response
